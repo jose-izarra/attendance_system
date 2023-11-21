384763abc39fbab947b2b95360f8ef1ac63e45eb
jose-izarra <jaiza0912@gmail.com>
2023-11-21 19:24:45 +0100

    connected to mysql database

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+### 1. Download dependencies
+### 2. Set environmental variables
+#### a. Create a .env file inside backend folder 
+#### b. Add the environmental variables there
+### 3. Cd into backend folder and do npm run dev
+
+
+
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,52 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-// setting up mysql
-const connection =  mysql.createConnection({
-    host: 'localhost',
-    user: 'dbuser',
-    password: 'grouppassword',
-    database: 'attendancedb'
-})
+const mysql = require('mysql');
 
-connection.connect()
+
+// connecting to mysql
+const db = mysql.createConnection({
+    user: process.env.mysql_USER,
+    host: process.env.mysql_HOST,
+    password: process.env.mysql_PASSWORD,
+    database: process.env.mysql_DATABASE
+});
+
+app.get("/select", (req, res) => {
+    db.query("SELECT * FROM  students", (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+
+});
+
+
+app.get("/insert", (req, res) => {
+    const email = 'zalsaheb.ieu2022@student.ie.edu';
+    const password = 'hispassword';
+    const myname = 'Zaid Nopicture'
+
+    db.query("INSERT INTO students (email, password, name) VALUES (?, ?, ?)",
+    [email, password, myname],
+    (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// connection.connect()
 
 
 app.get("/", (req, res) => {
     console.log("ok");
-    res.send('hi');
+    res.send('hello');
 })
 
 
-
-
-const port = 3000;
+const port = 3001;
 app.listen(port, () => console.log("Listening on Port "+ port));
-
-connection.end();
