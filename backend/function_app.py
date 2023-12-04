ce0e398556cb413e9fae2eb14e4377238b9beac6
jose-izarra <jaiza0912@gmail.com>
2023-12-04 21:42:45 +0100

    added login, signup, addNewCourse functions

--- backend/function_app.py
+++ backend/function_app.py
@@ -2,52 +2,213 @@ import azure.functions as func
 import logging
 import datetime
 import json
+import random
 import os
 import requests
-import pymysql
-from azure.servicebus import ServiceBusClient
+# from azure.servicebus import ServiceBusClient
 from time import sleep
+import mysql.connector
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="addToDB")
-def addToDB(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('ADDTODB Python HTTP trigger function processed a request.')
+db_details = {
+    "user": os.environ.get("user"),
+    "password" : os.environ.get("password"),
+    "host" : os.environ.get("host"),
+    "database": os.environ.get('db')
+}
+
+@app.route(route="singUp")
+def signUp(req: func.HttpRequest) -> func.HttpResponse:
+    rows = False  # just in case connection is unsuccessful
     
-    logging.info(os.environ['user'])
-    logging.info(os.environ['password'])
-    logging.info(os.environ['host'])
+    # sample call for this function
+    # http://localhost:7071/api/singUp?name=Jose%20Izarra&email=jaiza0912@gmail.com&password=mypassword&student=true
+    # change to student=false to append to professors table
     
-    user = os.environ.get("user")
-    password = os.environ.get("password")
-    host = os.environ.get("host")
-    database = os.environ.get("db")
-    
-    if user and password and host and database:
-        try: 
-            cnx = pymysql.connect(user=user, password=password,
-                                        host=host, database=database)
-            if cnx.open:
-                
-                cursor = cnx.cursor()
-                logging.info("=== Testing connection to database")
-                
-                query = 'SELECT * from CC_5;'
-                logging.info(query)
-                
-                rs=cursor.execute(query)
-                rs=cursor.fetchall()
-                logging.info(rs)
-            
-        except ValueError:
-            pass
-    else:
-        logging.info("Local settings variables couldn't be found")
+    try:
+        # connection = pymysql.connect(**db_details)
+        connection = mysql.connector.connect(**db_details)
         
-    if rs:
-        return func.HttpResponse(f"Successfully added. \n{rs}")
+        # load credentials to database if already doesnt exist
+        id_num = random.randint(0, 1000000000)
+        name = req.params.get('name')
+        email = req.params.get('email')
+        password = req.params.get('password')
+        student = req.params.get('student')
+        logging.info(f"------- name: {name}, email: {email}, password: {password}, student:{student}")
+        
+        
+        if connection.is_connected:
+            logging.info("------- Connected to database")
+            
+            cursor = connection.cursor()
+            
+            if student == "true":
+                query = f"""
+                            INSERT INTO students (student_id, student_name, student_email, student_password)
+                            VALUES (%s, %s, %s, %s);
+                        """
+            else: 
+                query = f"""
+                            INSERT INTO professors (professor_id, professor_name, professor_email, professor_password)
+                            VALUES (%s, %s, %s, %s);
+                        """
+            logging.info(f"------- QUERY: {query}")
+            
+            if id_num and name and email and password:
+                cursor.execute(query, (id_num, name, email, password))
+                connection.commit()
+                logging.info('-------- Query executed correctly')
+            
+            sel = "SELECT * from students"
+            cursor.execute(sel)
+            rows = cursor.fetchall()
+            
+            for row in rows:
+                logging.info(f'STUDENT TABLE: {row}')
+                
+    except mysql.connector.Error as e:
+        logging.info(f"Error {e}")
+    
+    finally:
+        if 'connection' in locals() and connection.is_connected:
+            cursor.close()
+            connection.close()
+            logging.info("MySQL connection closed")
+    
+    if rows:
+        return func.HttpResponse(f'Sign up successfull for credentials\n{rows}')
     else:
-        return func.HttpResponse(f"HTTP triggered successfully. Data not added", status_code=200)
+        # happens sometimes when data entered is repeated
+        return func.HttpResponse(f'Something didnt\' go well')  
+
+
+@app.route(route="login")
+def login(req: func.HttpRequest) -> func.HttpResponse:
+    rows = False
+    # Example of trigger call:
+    # http://localhost:7071/api/login?email=jaiza0912@gmail.com&password=mypassword&student=true
+    try: 
+        connection = mysql.connector.connect(**db_details)
+        
+        # get inputs
+        email = req.params.get('email')
+        password = req.params.get('password')
+        student = req.params.get('student')
+        
+        if connection.is_connected:
+            logging.info("------- Connected to database")
+            
+            cursor = connection.cursor()
+            
+            if student == 'true':
+                query = f"""
+                        SELECT student_email, student_password 
+                        FROM students WHERE student_email= %s AND student_password= %s;
+                        """
+            else:
+                query = f"""
+                        SELECT professor_email, professor_password 
+                        FROM professors WHERE professor_email= %s AND professor_password= %s;
+                        """
+            # sql_query = "SELECT column1, column2 FROM your_table_name WHERE column1 = %s AND column2 = %s"
+            if email and password:
+                cursor.execute(query, (email, password))
+            
+            rows = cursor.fetchall()
+            
+            # if exists, credentials are correct
+            if rows:
+                logging.info("------- Login credentials are correct")
+            else:
+                logging.info("------- Login credentials are incorrect")
+                
+    except mysql.connector.Error as e:
+        logging.info(f'------- Error: {e}')
+    
+    finally: 
+        if 'connection' in locals() and connection.is_connected:
+            cursor.close()
+            connection.close()
+            logging.info("MySQL connection closed")
+    if rows:
+        return func.HttpResponse(f'{rows}')
+    else:
+        return func.HttpResponse(f'Something didn\'t go well')
+            
+            
+     
+@app.route(route="addNewCourse")
+def addNewCourse(req: func.HttpRequest) -> func.HttpResponse:   
+    rows = False
+    # This function will be called by the teachers
+    
+    # Example call
+    #http://localhost:7071/api/addNewCourse?course_code=12345&course_name=Designing%20and%20Using%20Databases&prof_email=eduardo@gmail.com
+    try: 
+        connection = mysql.connector.connect(**db_details)
+        
+        # get inputs
+        course_id = random.randint(0, 1000000000)
+        course_code = req.params.get('course_code')
+        course_name = req.params.get('course_name')
+        prof_email = req.params.get('prof_email')
+        
+        if connection.is_connected:
+            logging.info("------- Connected to database")
+            
+            cursor = connection.cursor()
+            
+            # get professor id
+            
+            query = f"""
+                        SELECT professor_id 
+                        FROM professors WHERE professor_email= '{prof_email}';
+                    """
+            
+            cursor.execute(query)
+            
+            prof_id = cursor.fetchall()[0][0]
+            
+            if prof_id:
+                logging.info(f'------ prof_id: {prof_id}')
+            else:
+                logging.info(f'------ prof_id doesnt exist')
+            
+            query = """
+                    INSERT INTO courses (course_id, course_code, course_name, professor_id)
+                    VALUES (%s, %s, %s, %s);
+                    """
+                    
+            if course_id and course_code and course_name and prof_id:
+                cursor.execute(query, (course_id, course_code, course_name, prof_id))
+                connection.commit()
+                logging.info('-------- Query executed correctly')
+            else: 
+                logging.info('-------- Error entering inputs')
+            
+            sel = "SELECT * FROM courses;"
+            cursor.execute(sel)
+            rows = cursor.fetchall()
+            
+            for row in rows:
+                logging.info(f'COURSES TABLE: {row}')
+            
+    except mysql.connector.Error as e:
+        logging.info(f'------- Error: {e}')
+    
+    finally:
+        if 'connection' in locals() and connection.is_connected:
+            cursor.close()
+            connection.close()
+            logging.info("MySQL connection closed")
+            
+    if rows:
+        return func.HttpResponse(f'Successfully added to courses\n{rows}')
+    else:
+        return func.HttpResponse(f'Something didn\'t go well')
+    
 """
 name = req.params.get('name')
 if not name:
@@ -71,7 +232,7 @@ else:
 @app.route(route="createCode")
 def createCode(req: func.HttpRequest) -> func.HttpResponse:
     # this function will be called by the professor to initialize the code every 30 seconds
-    import random
+    
     logging.info('VERIFYCODE Python HTTP trigger function processed a request.')
     code = random.randint(100000, 999999)
     
@@ -97,7 +258,7 @@ def verifyCode(req: func.HttpRequest) -> func.HttpResponse:
 
 # def random_code_generator():
 #     return random.randint(100000, 999999)
-
+"""
 def send_code_to_frontend(code):
     response = requests.post("http://astro-website.com/api/sendcode", data={"code": code})
     return response.json()
@@ -138,3 +299,4 @@ def main():
     print(result)
     
 
+"""
