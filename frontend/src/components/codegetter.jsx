76a306517272c4ab356fd4ef8a1dea9bf7028ec3
haxy <zaidsaheb4@gmail.com>
2023-12-11 23:40:59 +0100

    fixed codegenerator

--- frontend/src/components/codegetter.jsx
+++ frontend/src/components/codegetter.jsx
@@ -9,17 +9,25 @@ const CodeGenerator = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError('');
+        setGeneratedCode('');
+
         try {
             const response = await sendCode(courseCode);
-            if (response.status === 200 && response.body) {
-                setGeneratedCode(response.body.code);
+            console.log('API Response:', response);
+
+            if (response.status === 200 && response.body && response.body.current_code) {
+                setGeneratedCode(response.body.current_code.toString()); // Convert to string if needed
             } else {
-                setError('Failed to fetch the generated code');
+                console.error('Invalid API Response:', response);
+                setError('Failed to fetch the generated code or invalid response');
             }
         } catch (err) {
+            console.error('API Error:', err);
             setError(err.message);
         }
     };
+
+
 
     return (
         <div className="flex flex-col items-start mr-10 max-w-xl w-full bg-gray-800 border border-gray-300 p-8 rounded-2xl mt-10 shadow-2xl">
@@ -32,10 +40,10 @@ const CodeGenerator = () => {
                     placeholder="Enter Course Code"
                     className="p-4 border border-gray-300 rounded text-black text-2xl"
                 />
+                <button type="submit" className="bg-blue-500 text-2xl text-white py-4 px-6 rounded mt-6 mr-auto">
+                    Get Code
+                </button>
             </form>
-            <button type="submit" className="bg-blue-500 text-2xl text-white py-4 px-6 rounded mt-6 mr-auto">
-                Get Code
-            </button>
             {generatedCode && <p className="mt-6 text-lg">Generated Code: {generatedCode}</p>}
             {error && <p className="mt-6 text-red-500">Error: {error}</p>}
         </div>
@@ -43,4 +51,3 @@ const CodeGenerator = () => {
 };
 
 export default CodeGenerator;
-
